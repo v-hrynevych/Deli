01fc032d8671d9473c2fe808125e84aececccf28
Volodymyr <vovkahrynevych@gmail.com>
2023-01-08 20:55:30 +0100

    edit env local

--- firebase.ts
+++ firebase.ts
@@ -2,20 +2,19 @@
 import "firebase/auth";
 import "firebase/firestore";
 import {initializeApp} from "firebase/app";
-import { getStorage } from "firebase/storage";
+import {getStorage} from "firebase/storage";
 import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 
-
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-    databaseURL: process.env.NEXT_PUBLIC_DATA_BASE_URL,
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATA_BASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID,
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
